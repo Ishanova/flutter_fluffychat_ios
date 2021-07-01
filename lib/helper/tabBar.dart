31a8f30567ca64e050281ce0c788cafe473f0030
Ishanova <Ishanovanr@mail.ru>
2021-07-01 16:50:49 +0300

    01.07 - 1

--- lib/helper/tabBar.dart
+++ lib/helper/tabBar.dart
@@ -5,25 +5,40 @@ import 'package:flutter_fluffychat_ios/test_data/test_data.dart';
 
 class TabBar extends StatelessWidget {
 
+  final listFolders = ['Все', 'Непрочитанные', 'Учёба', 'Что-то'];
+
+  final addBtn = Container(
+    margin: EdgeInsets.symmetric(horizontal: 16),
+    child: Icon(
+      CupertinoIcons.slider_horizontal_3,
+      size: 22,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
+    final List<Container> folders = listFolders.map((element) {
+      return Container(child: ChatTab(element, true, 6, false));
+    }).toList();
+    folders.add(addBtn);
     return Container(
       color: white,
       height: 80,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: [
-          ChatTab("Все", true, 6, false),
-          ChatTab("Непрочитанные", true, 2, true),
-          ChatTab("Учёба", false, 2, true),
-          Container(
+        children: folders,
+          //(this.tabName, this.isUnread, this.counter, this.isTagged)
+          // ChatTab("Все", true, 6, false),
+          // ChatTab("Непрочитанные", true, 2, true),
+          // ChatTab("Учёба", false, 2, true),
+          /*Container(
               margin: EdgeInsets.symmetric(horizontal: 16),
               child: Icon(
                   CupertinoIcons.slider_horizontal_3,
                 size: 22,
               ),
-          ),
-        ],
+          ),*/
+        // ],
       ),
     );
     /*
--- lib/view/chat/chatTab.dart
+++ lib/view/chat/chatTab.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_fluffychat_ios/helper/indicator.dart';
 import 'package:flutter/rendering.dart';
 
 class ChatTab extends StatelessWidget{
   String tabName;
-  bool isUnread, isTagged;
+  bool isUnread, isTagged/*, active = false*/;
   int counter;
 
   ChatTab (this.tabName, this.isUnread, this.counter, this.isTagged);
@@ -12,13 +13,24 @@ class ChatTab extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {print("DOUBLE TAB");},
+      onTap: () {
+        // active = true;
+        print(tabName);
+        },
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 16),
+        /*decoration: !active ? BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.5, color: Colors.blue[300]),
+            ),
+        ) : null,*/
         child: Row(
           children: [
             Text(tabName,
-                style: TextStyle(fontSize: 16, fontWeight:  FontWeight.w500)),
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight:  FontWeight.w500
+                )),
             isUnread ? Indicator(1, counter) : Text(""),
             //isUnread ? Indicator(0) : Text(""),
           ],
